48eeffd7ecce5e700e6613564e374fc2d2ab8722
Evelyn Yang <changminyang@MacBook-Pro-72.local>
2020-04-26 23:35:38 -0700

    Updates

---
+++ precache-manifest.e82b67ab5920b1efda64a93951d27b85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09981b337affd389cb30a624e10ee56f",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2d3734a60057df60a77b",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afda21390b6b4cb282a8",
+    "url": "/test/static/js/2.262eeec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.262eeec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3734a60057df60a77b",
+    "url": "/test/static/js/main.76d47be5.chunk.js"
+  },
+  {
+    "revision": "efff10f7199ae425f78c",
+    "url": "/test/static/js/runtime-main.d8e83e74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
