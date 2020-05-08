7d52fbfd7c0a4633507081c1b4652c42a1120373
Evelyn Yang <changminyang@MacBook-Pro-72.local>
2020-05-07 18:34:36 -0700

    ey - assignment 5

---
+++ src/config.js
@@ -0,0 +1,7 @@
+//dont worry about exposing your API key, it is bad practice and generally not advised, but for now it really doesn't matter, Google ensures only the authorized users can access api keys
+const config = {
+    apiKey: "AIzaSyAy6LzfpyHJqY8omVCku4Sn_o1tVcxUpVo",
+    authDomain: "portfolio-3c123.firebaseapp.com",
+    databaseURL: "https://portfolio-3c123.firebaseio.com",
+  }
+export default config;
